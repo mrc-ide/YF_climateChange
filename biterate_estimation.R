a83559c76d733fbf37a126e67be7769897a75b25
kgaythorpe <k.gaythorpe@googlemail.com>
2018-08-21 13:23:10 +0100

    script to estimate bite rate parameters

---
+++ biterate_estimation.R
@@ -0,0 +1,171 @@
+
+run_id = 1
+### ESTIMATE BITE RATE ###
+
+library(ggplot2)
+library(dplyr)
+
+### DATA ###
+
+mordecai_biterate <- read_csv("Z:/YF_climateChange/Data/mordecai_biterate.csv")
+hamlet_biterate = read_csv("Z:/YF_climateChange/Data/hamlet_biterate.csv")
+
+mordecai_biterate$author = "mordecai"
+hamlet_biterate$author = "hamlet"
+
+names(hamlet_biterate) = names(mordecai_biterate)
+
+biterate = rbind(mordecai_biterate, hamlet_biterate)
+
+################################################################
+### FUNCTIONS ###
+################################################################
+source('Z:/YF_climateChange/FUNCTIONS/temp_suitability.R')
+source('Z:/YF_climateChange/FUNCTIONS/GLMonly_functions.R')
+
+likelihood = function(param, dat, sigma = NA){
+  
+  T0 = as.numeric( param[1] )
+  Tm = as.numeric( param[2])
+  c = as.numeric( param[3])
+  
+  Temp = dat$T
+  
+  a = briere(Temp, T0, Tm , c)
+  a[a<=0] = 1e-4
+  
+  if(length(unique(dat$author))>1 ){
+    
+    LL = sum(  dexp(dat$bite_rate, rate = a, log = TRUE) )
+  } else {
+    LL = sum( log( dnorm(dat$bite_rate, mean = a, sd = sigma)) )
+  }
+  
+  return(LL)
+}
+################################################################
+prior = function(param){
+  
+  T0 = as.numeric(param[1] )
+  Tm = as.numeric(param[2])
+  c = as.numeric(param[3])
+  
+  prior_prob = log( dnorm(T0, mean = 13.35, sd = 2) ) + log( dnorm(Tm, mean = 40.08, sd = 0.05) ) +
+               log( dnorm(c, mean = 2.02e-4, sd = 2e-5))
+  
+  return(prior_prob)
+}
+################################################################
+MCMC_step = function(param, data, chain_cov, adapt, accCurrent){
+  
+  #new param
+  param_prop = proposal(as.numeric(param), chain_cov, adapt)
+  names(param_prop) = names(param)
+  
+  #prior_prop
+  prior_prop = prior( exp( param_prop) )
+  
+  #if fintie calc likelihood
+  if(is.finite(prior_prop)){
+    like_prop = likelihood( exp( param_prop ), data, sigma=NA)
+    
+    accProp = like_prop + prior_prop + sum(param_prop) #correction for log scale
+    
+    p_accept= accProp - accCurrent
+    if(is.na(p_accept) ){ p_accept = -Inf}
+    
+  } else {
+    p_accept = log(0)
+  }
+  
+  ## accept/reject step:
+  tmp = (runif(1))
+  if( tmp< min( exp(p_accept), 1)  ) { # accept:
+    param = param_prop
+    accCurrent = accProp
+    accept = 1
+  } else { # reject:
+    
+    accept = 0
+  }
+  
+  return(list(param = param,  accCurrent = accCurrent, accept = accept)  )
+}
+
+
+#########################################################################################################
+### Setup ###
+#########################################################################################################
+pars_ini = log( data.frame("T0" = 13.35,
+                      "Tm" = 40.08,
+                      "c" = 2e-4) )
+
+
+#########################################################################################################
+### MCMC ###
+#########################################################################################################
+
+### find posterior probability at start ###
+out = MCMC_step(pars_ini, 
+                biterate,
+                chain_cov=1,
+                adapt=0, 
+                accCurrent=-Inf)
+
+#create a directory to save the output in
+date=format(Sys.time(),"%Y%m%d")
+name_dir = paste0("biterate_MCMC_chain", "_", date)
+if(!dir.exists(name_dir)) dir.create(name_dir,  showWarnings = TRUE)
+
+
+#plain old mcmc on fixed model prob
+Niter = 1e5
+chain = posteriorProb = acceptRate = NULL
+burnin = 50
+fileIndex = 0
+iter = 1
+
+for (iter in iter:Niter){
+  #save current step
+  param = out$param
+  names(param) = names(pars_ini)
+  accCurrent = out$accCurrent
+  accept = out$accept
+  
+  chain = rbind(chain, param)
+  posteriorProb = rbind(posteriorProb, accCurrent)
+  acceptRate = rbind(acceptRate, accept)
+  
+  
+  if(iter>100 & run_id<100) plot(chain[,1] , type= "l") #plot(posteriorProb, type = 'l')
+  
+  if (iter %% 1000 == 0){
+    
+    colnames(acceptRate) = "acceptRate"
+    colnames(posteriorProb) = "posteriorProb"
+    if (iter %% 10000 == 0){
+      fileIndex  = iter/10000
+    }
+    write.csv(cbind(chain,  posteriorProb, acceptRate)[min((fileIndex * 10000+1),iter):iter,], 
+              paste0(name_dir,"/","chain",fileIndex,"_output",".csv") ,row.names = FALSE) 
+  }
+  
+  
+  #adapt?
+  if (iter>burnin & runif(1)<0.9){ #adapt
+    adapt = 1
+    chain_cov  = cov(chain[max(nrow(chain)-10000, 1):nrow(chain),])
+  } else {
+    adapt = 0
+    chain_cov = 1
+  }
+  
+  #new step
+  out = MCMC_step(param,
+                      biterate,
+                      chain_cov,
+                      adapt, 
+                      accCurrent)
+  
+  
+}
