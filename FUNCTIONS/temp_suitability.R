ed79d190f1eae66ef19cab8e278807549a20019e
kgaythorpe <k.gaythorpe@googlemail.com>
2018-08-22 12:22:26 +0100

    Added new estimated values for temp suitability

--- FUNCTIONS/temp_suitability.R
+++ FUNCTIONS/temp_suitability.R
@@ -66,14 +66,14 @@ temp_suitability_hamlet = function(Temp){
   #using mean values of mordecai
   
   #bite rate
-  a = briere(Temp, T0=13.35, Tm=40.08, c=2.02e-4) # Mordecai Ae aegypti
+  a = briere(Temp, T0=8.311805, Tm=10.10016, c=2.27e-4) # new estimate medians
   
   # mu = m mortality
-  mu = 1/ ifelse( quad(Temp, T0=9.16, Tm=37.73, c=-1.48e-1)<=0, 1, quad(Temp, T0=9.16, Tm=37.73, c=-1.48e-1) ) #guard against negatives and zeros
+  mu = 1/ ifelse( quad(Temp, T0=10.98518, Tm=38.39435, c=-0.30234)<=0, 1, quad(Temp, T0=10.98518, Tm=38.39435, c=-0.30234) ) #guard against negatives and zeros
   
   
   # PDR = parasite development rate = 1/EIP  
-  PDR = briere(Temp, T0=18.3, Tm=42.3, c=0.000174) #ZIKv
+  PDR = briere(Temp, T0=18.61395, Tm=42.19607, c=1.51e-4) #ZIKv
   
   a[is.na(a)] = PDR[is.na(PDR)] = 0
   
